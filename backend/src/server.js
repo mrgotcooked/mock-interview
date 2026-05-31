431f9bf42858dc934bea85a3f2de3a1bf644fee5
advitya <advitya9@gmail.com>
2026-05-31 13:29:36 +0530

    Remove frontend serving from backend deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,38 +21,7 @@ app.get("/api", (req, res) => {
   });
 });
 
-// deployment setup
-if (ENV.NODE_ENV === "production") 
-  
-//   why?
-// Because during deployment:
 
-// frontend is already built
-// backend serves frontend files
-
-// But during development:
-
-// Vite runs separately on port 5173
-// Express runs separately on 3000
-  
-  
-  {
-
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));//dist? Vite converts React code into optimized HTML/CSS/JS files.
-  //express.static? It tells Express: "If browser asks for frontend files, serve them."
-//  Example:
-
-// Browser requests:
-// assets/index-abc123.js
-// Express finds it inside:
-// frontend/dist/assets
-// and sends it.
-  app.get("/{*any}", (req, res) => { //means:for any route not matched above:
-    res.sendFile(
-      path.resolve(__dirname, "../frontend" , "dist", "index.html")//This sends: frontend/dist/index.html to browser ,{/*go one folder up in frontend */}
-    );
-  });
-}
 connectDB().then(()=>{
   app.listen(ENV.PORT, () =>//starting server
   console.log(`server is running on port ${ENV.PORT}`)  
