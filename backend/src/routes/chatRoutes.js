df2aaec91122ff7040cfdc54445ff307b9a28f72
advitya <advitya9@gmail.com>
2026-06-01 16:58:58 +0530

    fixes in server side auth

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import { getStreamToken } from "../controllers/chatController";
-import { protectRoute } from "../middleware/protectRoute";
+import { getStreamToken } from "../controllers/chatController.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 router.get("/token",protectRoute,getStreamToken);
 export default router
